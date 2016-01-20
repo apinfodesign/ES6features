340980d954e394023edc286156af06c24dbc59aa
Miles Hochstein <miles.hochstein@gmail.com>
2016-01-20 13:17:27 -0800

    various es6 syntax examples

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp    = require('gulp');
+var mocha   = require('gulp-mocha');
+const babel = require('gulp-babel');
+
+var gutil   = require('gulp-util');
+var jshint  = require('gulp-jshint');
+
+require('babel-core/register');
+
+gulp.task('test', function () {
+    return gulp.src( 'test/**/*.js', { read: false } )
+        .pipe(mocha({
+            reporter: 'nyan',
+            compilers: [
+                'js:babel-core/register',
+            ]
+        }));
+});
+
+
+gulp.task('build', () => {
+    return gulp.src('src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('mocha', function() {
+    return gulp.src(['test/*.js'], {read: false})
+        .pipe(mocha({reporter: 'list'}))
+        //.on('error', handleError);
+        .on('error', gutil.log);
+});
+
+
+gulp.task('lint', function() {
+    return gulp.src(['*.js', 'test/*.js'], {read: false})
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+
+gulp.task('watch-mocha', function() {
+    gulp.watch(['./*.js', 'test/**','!package.json'], ['lint','mocha']);
+});
+
+
+gulp.task('default', ['lint', 'mocha']);
+
+gulp.task('testing', ['lint', 'mocha', 'watch-mocha']);
